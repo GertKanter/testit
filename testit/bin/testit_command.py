d2258028dc93dbbfd5afbc34df38f28b483e16ab
Gert Kanter <gert.kanter@robokratt.xyz>
2018-02-14 12:31:01 +0200

    gitignore

---
+++ testit/bin/testit_command.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2018 Gert Kanter.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Gert Kanter
+
+import rospy
+import rospkg
+import argparse
+import testit.testit_common
+import testit.srv
+
+class TestIt:
+    def __init__(self, opt):
+        self.opt = opt
+        rospy.wait_for_service('testit/bringup')
+        rospy.wait_for_service('testit/teardown')
+        rospy.wait_for_service('testit/status')
+        rospy.wait_for_service('testit/test')
+        self.bringup_service = rospy.ServiceProxy('testit/bringup', testit.srv.Command)
+        self.teardown_service = rospy.ServiceProxy('testit/teardown', testit.srv.Command)
+        self.status_service = rospy.ServiceProxy('testit/status', testit.srv.Command)
+        self.test_service = rospy.ServiceProxy('testit/test', testit.srv.Command)
+
+    def execute(self, command):
+        """Execute the command.
+
+        command - start runs the pipeline(s) bringup(s)
+        """
+        getattr(self, command)()
+
+    def call_service(self, service):
+        try:
+            response = service("".join(self.opt.pipeline))
+            print response
+        except rospy.ServiceException, e:
+            rospy.logerr("Calling service failed: %s" % e)
+
+    def bringup(self):
+        rospy.loginfo("Bringing up pipeline(s)...")
+        self.call_service(self.bringup_service)
+
+    def teardown(self):
+        rospy.loginfo("Tearing down pipeline(s)...")
+        self.call_service(self.teardown_service)
+
+    def reload(self):
+        rospy.loginfo("Reloading configuration to ROS parameter server...")
+        testit_common.load_config_to_rosparam(testit_common.parse_yaml(opt.config))
+
+    def status(self):
+        self.call_service(self.status_service)
+
+    def test(self):
+        self.call_service(self.test_service)
+
+
+if __name__ == '__main__':
+    rospy.init_node('testit_cmdline', anonymous=True, disable_signals=True)
+    rospack = rospkg.RosPack()
+    parser = argparse.ArgumentParser(description="TestIt Command Line Interface")
+    parser.add_argument("command", choices=["bringup", "test", "teardown", "status", "log", "bag", "reload"])
+    parser.add_argument("-c", "--config", action="store", default=rospack.get_path('testit')+'/cfg/config.yaml',
+                    help="Configuration file location")
+    parser.add_argument("-d", "--docker", action="store", default='testitros/testit:latest',
+                    help="TestIt docker image tag")
+    parser.add_argument("pipeline", nargs="*")
+    opt = parser.parse_args(rospy.myargv()[1:])
+    
+    testit = TestIt(opt)
+    testit.execute(opt.command)
---
+++ testit/src/testit/testit_daemon.py
@@ -0,0 +1,294 @@
+#!/usr/bin/env python
+
+# Software License Agreement (BSD License)
+#
+# Copyright (c) 2018 Gert Kanter.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+#  * Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+#  * Redistributions in binary form must reproduce the above
+#    copyright notice, this list of conditions and the following
+#    disclaimer in the documentation and/or other materials provided
+#    with the distribution.
+#  * Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived
+#    from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+# Author: Gert Kanter
+
+import rospy
+import testit.srv
+import rosparam
+import rospkg
+import testit_common
+import threading
+import time
+import sys
+import os
+import re
+
+class TestItDaemon:
+    def __init__(self):
+        rospy.Service('testit/bringup', testit.srv.Command, self.handle_bringup)
+        rospy.Service('testit/teardown', testit.srv.Command, self.handle_teardown)
+        rospy.Service('testit/status', testit.srv.Command, self.handle_status)
+        rospy.Service('testit/test', testit.srv.Command, self.handle_test)
+        self.initialize()
+
+    def initialize(self):
+        self.load_config_from_file()
+        self.threads = {}
+        self.test_threads = {}
+        self.testing = False
+        self.tests = self.rosparam_list_to_dict(rospy.get_param('testit/tests', None), 'tag')
+        if self.tests is None:
+            rospy.logerror("No tests defined in configuration!")
+            sys.exit(-1)
+        self.pipelines = self.rosparam_list_to_dict(rospy.get_param('testit/pipelines', None), 'tag')
+        if self.pipelines is None:
+            rospy.logerror("No pipelines defined in configuration!")
+            sys.exit(-1)
+        if rospy.get_param('testit/configuration', None) is None:
+            rospy.logerror("No configuration defaults defined in configuration!")
+            sys.exit(-1)
+        self.pipelines = self.substitute_config_values(
+                           self.set_defaults(self.pipelines, 
+                                             rospy.get_param('testit/configuration')))
+
+    def substitute_config_values(self, params):
+        for param in params:
+            for key in params[param]:
+                m = re.findall('(\[\[.*?\]\])', str(params[param][key]))
+                if m is not None:
+                    for replacement in m:
+                        params[param][key] = params[param][key].replace(replacement, params[param][replacement[2:-2]], 1)
+        return params
+
+
+    def set_defaults(self, params, defaults):
+        for param in params:
+            for key in params[param]:
+                if params[param][key] == '':
+                    # set default
+                    params[param][key] = defaults[key]
+        return params
+
+    def rosparam_list_to_dict(self, param, key):
+        if param is None:
+            return
+        return_value = {}
+        for item in param:
+            return_value[item[key]] = item
+        return return_value
+
+    def load_config_from_file(self):
+        rospack = rospkg.RosPack()
+        filename = rospy.get_param('~config')
+        rospy.loginfo("Loading configuration from " + filename + "...")
+        testit_common.load_config_to_rosparam(testit_common.parse_yaml(filename))
+
+    def thread_worker(self, tag, prefix):
+        rospy.loginfo('[%s] Starter thread started!' % tag)
+        rospy.loginfo('[%s] Executing %s SUT...' % (tag, prefix))
+        os.system(self.pipelines[tag][prefix + 'SUT'])
+        rospy.loginfo('[%s] Done!' % tag)
+        rospy.loginfo('[%s] Executing %s TestIt...' % (tag, prefix))
+        os.system(self.pipelines[tag][prefix + 'TestIt'])
+        rospy.loginfo('[%s] Done!' % tag)
+        rospy.loginfo('[%s] Waiting for the %s to finish...' % (tag, prefix))
+        start_time = rospy.Time.now()
+        while self.pipelines[tag][prefix + 'SUTDelay'] == 0 or (rospy.Time.now() - start_time).to_sec() < self.pipelines[tag][prefix + 'SUTDelay']:
+            if self.pipelines[tag][prefix + 'SUTFinishTrigger'] != '-':
+                # TODO using timeout + trigger
+                pass
+            time.sleep(1.0) 
+        rospy.loginfo('[%s] Done!' % tag)
+        self.threads[tag]['result'] = True
+        return True
+
+    def multithreaded_command(self, verb, req, prefix):
+        rospy.logdebug(verb + " requested")
+	if req.pipeline == "":
+            rospy.loginfo(verb + " all pipelines...")
+        else:
+            rospy.loginfo(verb + "ing " + req.pipeline + "...")
+        for pipe in rospy.get_param('testit/pipelines', []):
+            if req.pipeline == '' or req.pipeline == pipe['tag']:
+                rospy.loginfo(pipe['tag'] + " " + verb.lower() + "ing...")
+                thread = threading.Thread(target=self.thread_worker, args=(pipe['tag'], prefix))
+                self.threads[pipe['tag']] = {'thread': thread, 'result': None}
+                thread.start()
+        result = True
+        message = ""
+        while len(self.threads) > 0:
+            for thread in self.threads:
+                if not self.threads[thread]['result'] is None:
+                    msg = '%s finished with %r' % (thread, self.threads[thread]['result'])
+                    rospy.loginfo(msg)
+                    if not self.threads[thread]['result']:
+                        # Report if experienced a failure...
+                        message += msg + "\n"
+                        self.pipelines[thread][prefix] = False
+                        result = False
+                    else:
+                        # Success!
+                        self.pipelines[thread][prefix] = True
+                    del self.threads[thread]
+                    break
+            time.sleep(1.0)
+            rospy.loginfo_throttle(15.0, '...')
+        return (result, message)
+
+    def handle_bringup(self, req):
+        result = self.multithreaded_command("Start", req, "bringup")
+        return testit.srv.CommandResponse(result[0], result[1])
+
+    def handle_teardown(self, req):
+        result = self.multithreaded_command("Stop", req, "teardown")
+        return testit.srv.CommandResponse(result[0], result[1])
+
+    def handle_status(self, req):
+        rospy.logdebug("Status requested")
+        message = ""
+        result = True
+        try:
+            for pipeline in self.pipelines: # dict
+                if self.pipelines[pipeline].get('bringup', False):
+                    message += "[%s] ONLINE\n" % self.pipelines[pipeline]['tag']
+                else:
+                    message += "[%s] OFFLINE\n" % self.pipelines[pipeline]['tag']
+        except:
+            result = False
+        return testit.srv.CommandResponse(result, message)
+
+    def acquire_pipeline(self, tag):
+        """
+        Return:
+        pipeline tag
+        """
+        rospy.loginfo("Acquiring pipeline for test \'%s\'" % tag)
+        while True:
+            for pipeline in self.pipelines:
+                if self.pipelines[pipeline].get('bringup', False) and not self.pipelines[pipeline].get('busy', False):
+                    self.pipelines[pipeline]['busy'] = True
+                    return pipeline
+            time.sleep(0.5)
+            rospy.logwarn_throttle(30.0, 'Test \'%s\' waiting for a free pipeline...' % tag)
+
+    def execute_system(self, pipeline, system, mode):
+        """
+        blocking
+        Returns:
+        true -- if successful, false otherwise
+        """
+        rospy.loginfo("Executing...")
+        os.system(self.pipelines[pipeline][mode + system])
+        start_time = rospy.Time.now()
+        while self.pipelines[pipeline][mode + system + 'Delay'] == 0 or (rospy.Time.now() - start_time).to_sec() < self.pipelines[pipeline][mode + system + 'Delay']:
+            if self.pipelines[pipeline][mode + system + 'FinishTrigger'] != '-':
+                # TODO using timeout + trigger
+                pass
+            time.sleep(1.0) 
+        rospy.loginfo('Execution done!')
+        return True
+
+    def execute_in_testit_container(self):
+        """
+        Returns:
+        True if test successful, False otherwise
+        """
+        return True
+
+    def test_thread_worker(self, tag):
+        """
+        Arguments:
+            tag -- test tag (string)
+        """
+        rospy.loginfo("%s tag testthreadwork start" % tag)
+        pipeline = self.acquire_pipeline(tag) # find a free pipeline (blocking)
+        rospy.loginfo("Acquired pipeline %s" % pipeline)
+        # runSUT
+        rospy.loginfo("Running SUT...")
+        if self.execute_system(pipeline, 'SUT', 'run'):
+            # runTestIt
+            rospy.loginfo("Running TestIt...")
+            if self.execute_system(pipeline, 'TestIt', 'run'):
+                # launch test in TestIt docker in new thread (if oracle specified, run in detached mode)
+                # execute oracle in TestIt docker in new thread (if specified)
+                rospy.loginfo("Executing tests in TestIt container...")
+                self.test_threads[tag]['result'] = self.execute_in_testit_container()
+                # stopTestIt
+                rospy.loginfo("Stopping TestIt container...")
+                self.execute_system(pipeline, 'TestIt', 'stop')
+            else:
+                # unable to run TestIt
+                rospy.logerr("Unable to run TestIt!")
+                pass
+            # stopSUT
+            rospy.loginfo("Stopping SUT...")
+            self.execute_system(pipeline, 'SUT', 'stop')
+        else:
+            # Unable to run SUT
+            rospy.logerr("Unable to run SUT!")
+            pass
+        rospy.loginfo("Freeing pipeline \'%s\'" % pipeline)
+        self.pipelines[pipeline]['busy'] = False
+
+    def nonblocking_test_monitor(self):
+        rospy.loginfo("i am going to set self.testing false sometime soon")
+        while True:
+            sleep = True
+            for thread in self.test_threads:
+                if not self.test_threads[thread]['result'] is None:
+                    del self.test_threads[thread]
+                    sleep = False
+                    break
+            if len(self.test_threads) == 0:
+                self.testing = False
+                return # all threads are finished
+            if sleep:
+                time.sleep(0.5)
+
+    def handle_test(self, req):
+        rospy.logdebug("Test requested")
+        result = True
+        message = ""
+        if not self.testing:
+            self.testing = True
+            for test in self.tests: # key
+                #rospy.loginfo(pipe['tag'] + " " + verb.lower() + "ing...")
+                thread = threading.Thread(target=self.test_thread_worker, args=(test,))
+                self.test_threads[test] = {'thread': thread, 'result': None}
+                thread.start()
+            thread = threading.Thread(target=self.nonblocking_test_monitor)
+            thread.start()
+        else:
+            rospy.logerr("Unable to start tests! Tests are already executing!")
+        #result = self.multithreaded_command("", req, "test")
+        return testit.srv.CommandResponse(result, message)
+
+if __name__ == "__main__":
+    rospy.init_node('testit_daemon')
+    testit_daemon = TestItDaemon()
+    rospy.loginfo("TestIt daemon started...")
+    while not rospy.is_shutdown():
+        pass
+    rospy.loginfo("Shut down everything!")
