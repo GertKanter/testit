7208249759a62b4d6b31ee4faeab2edee08ec53b
Gert Kanter <gert.kanter@robokratt.xyz>
2019-05-16 17:30:11 +0300

    Refactor to containers to 'latest' tag

--- testit/scripts/build_local_testit_docker.sh
+++ testit/scripts/build_local_testit_docker.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-QUERY=$(docker images -q testitros/testit:base)
+QUERY=$(docker images -q testitros/testit:latest)
 if [ ${#QUERY} -eq 0 ]; then
-  echo "Building testitros/testit:base container..."
+  echo "Building testitros/testit:latest container..."
   cd $(rospack find testit)/docker
-  docker build --no-cache -t testitros/testit:base .
+  docker build --no-cache -t testitros/testit:latest .
 fi
