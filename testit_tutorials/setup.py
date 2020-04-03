5677c71d35ae7762d4f0329a857d14c6324cffe6
ingmar <ingmar@liibert.ee>
2020-04-03 17:56:37 +0300

    #22 testit_dtron

--- testit_tutorials/setup.py
+++ testit_tutorials/setup.py
@@ -3,8 +3,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup()
-d['packages'] = ['testit_tutorials']
-#d['package_dir'] = {'': 'src'}
+d = generate_distutils_setup(packages=[],
+                             package_dir={})
 
 setup(**d)
