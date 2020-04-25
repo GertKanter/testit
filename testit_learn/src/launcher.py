e8cc7223f194c42c0ec82c1027055904669de4d7
ingmar <ingmar@liibert.ee>
2020-04-26 01:06:03 +0300

    Testit learn

--- testit_learn/src/launcher.py
+++ testit_learn/src/launcher.py
@@ -16,8 +16,8 @@ class Launcher:
         self.test_config = None
         self.test_tag = None
 
-        self.read_config()
         self.read_test_config()
+        self.read_config()
 
     def read_config(self):
         logger_config_path = self.test_config['loggerConfiguration']
