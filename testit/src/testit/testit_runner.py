8af8a5e0b89a59c0e906856dd33579512ff0fc3e
ingmar <ingmar@liibert.ee>
2020-05-02 18:13:24 +0300

    Testit learn

--- testit/src/testit/testit_runner.py
+++ testit/src/testit/testit_runner.py
@@ -109,7 +109,7 @@ class TestItRunner:
                 break
             data = self.optimizer.state_hashes[next_step[0]][1]
             for state_hash in self.optimizer.state_hashes[next_step[0]][0]:
-                state = json.loads(self.optimizer.state_hashes[next_step[0]][0][state_hash].replace("u", "").replace("'", '"'))
+                state = json.loads(self.optimizer.state_hashes[next_step[0]][0][state_hash].replace("u'", "").replace("'", '"'))
                 rospy.loginfo(str(state))
                 rospy.loginfo(str(tuple(sorted(list(state.keys())))))
                 rospy.loginfo(str(tuple(sorted(list(data.keys())))))
