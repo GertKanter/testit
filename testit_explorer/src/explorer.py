4d1d71114508ed03d386ba29dea9091a241a0e6c
ingmar <ingmar@liibert.ee>
2020-05-06 00:10:26 +0300

    Testit learn

--- testit_explorer/src/explorer.py
+++ testit_explorer/src/explorer.py
@@ -14,7 +14,7 @@ from testit_learn.msg import StateMachine
 from testit_learn.srv import StateMachineToUppaal, StateMachineToUppaalResponse, StateMachineToUppaalRequest
 
 
-import testit_explorer.classes
+from testit_explorer.srv import *  # This is necessary for imports to work
 from classes.Action import Action
 from classes.ExploreMoveStrategy import ExploreMoveStrategy
 from classes.ModelRefinementMoveStrategy import ModelRefinementMoveStrategy
